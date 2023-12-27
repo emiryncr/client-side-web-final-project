dd01ebec1e167102f4e7d8489923da5cb63758e1
Emir Yeniçeri <emiryncr@gmail.com>
2023-12-27 12:42:22 +0300

    all files uploaded
    loaded after done

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (!navigator.onLine) {
+        alert("Please connect to the internet to access external links and APIs.");
+    }
+});
